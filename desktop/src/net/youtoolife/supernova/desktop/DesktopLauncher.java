62c7c3fe89f57a21ec233b21ccee84088dbf04d0
YouTooLife <youtoolifesite@ya.ru>
2016-07-13 16:42:33 +0700

    Launcher

--- desktop/src/net/youtoolife/supernova/desktop/DesktopLauncher.java
+++ desktop/src/net/youtoolife/supernova/desktop/DesktopLauncher.java
@@ -1,15 +1,17 @@
 package net.youtoolife.supernova.desktop;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import net.youtoolife.supernova.Supernova;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 768;
+		//config.width = 1280;
+		//config.height = 768;
+		config.width = config.getDesktopDisplayMode().width;
+		config.height = config.getDesktopDisplayMode().height;
 		config.fullscreen = true;
 		new LwjglApplication(new Supernova(), config);
 	}
